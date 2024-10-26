8f68371cefa5600d9e3807590e32e2f19e4a7e94
Student <mpodzielny18@gmail.com>
2024-10-26 13:45:32 +0200

    podaj liczbe poprawione

--- kurwamac.java
+++ kurwamac.java
@@ -1,12 +1,12 @@
-import java.until.Scanner;
+import java.util.Scanner;
 
-public.class App {
+public class kurwamac {
     public static void main(String[] args) {
 
         Scanner input=new Scanner(System.in);
         System.out.print("Podaj liczbę: ");
         int liczba=input.nextInt();
 
-        System.out.println("Podana liczba to: " + liczba)
+        System.out.println("Podana liczba to: " + liczba);
     }
 }
