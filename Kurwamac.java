d67e637c35d90f771f231e816db767a251dadce8
Kaliszek47 <nkaliszek@gmail.com>
2024-10-26 14:06:59 +0200

    ulepszone

--- Kurwamac.java
+++ Kurwamac.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class kurwamac {
+public class Kurwamac {
     public static void main(String[] args) {
 
         Scanner input=new Scanner(System.in);
@@ -8,5 +8,19 @@ public class kurwamac {
         int liczba=input.nextInt();
 
         System.out.println("Podana liczba to: " + liczba);
+
+        final int STALA = 69;
+
+        if (liczba>STALA) {
+            System.out.println("Twoja liczba jest większa niż " + STALA);
+        }
+        else if (liczba<STALA) {
+            System.out.println("Twoja liczba jest mniejsza niż " + STALA);
+        }
+        else {
+            System.out.println("Twoja liczba jest równa " + STALA);
+        }
     }
 }
+// stała "final int" wielkimi literami i podłogą "STALA_ZMIENNA"
+// sout = System.out.println
