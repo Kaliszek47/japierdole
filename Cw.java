4afd71a685221e0fb62744c5463a2bd6f9f09099
Kaliszek47 <nkaliszek@gmail.com>
2024-10-26 15:53:43 +0200

    Update Cw.java

--- Cw.java
+++ Cw.java
@@ -1,5 +1,16 @@
+// 1.8 * celc + 32
 import java.util.Scanner;
 
 public class Cw {
-  public status
+  public status void main(String[] args) {
+
+         System.out.print("Program konwertujący wartość temperatury z stopni Celcjusza do Fahrenheita");
+
+        Scanner dane=new Scanner(System.in);
+        System.out.print("Podaj wartość temperatury w Celcjuszach ");
+        float temp=dane.nextInt();
+
+        float farhe = (celcjusz * 1.8) + 32;
+
+          System.out.print("wartość tej temperatury w Fahrenheitach wynosi " + farhe");
 }
