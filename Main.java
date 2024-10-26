1b6c4a35ace4372445369bdd7c2b1de146fba1c3
Kaliszek47 <nkaliszek@gmail.com>
2024-10-26 14:21:19 +0200

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,11 +1,18 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<module type="JAVA_MODULE" version="4">
-  <component name="NewModuleRootManager" inherit-compiler-output="true">
-    <exclude-output />
-    <content url="file://$MODULE_DIR$">
-      <sourceFolder url="file://$MODULE_DIR$/src" isTestSource="false" />
-    </content>
-    <orderEntry type="inheritedJdk" />
-    <orderEntry type="sourceFolder" forTests="false" />
-  </component>
-</module>
+import java.util.Scanner;
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner wpis= new Scanner(System.in);
+        System.out.println("Podaj liczbę :");
+        int liczba= wpis.nextInt();
+
+        for(int i=0; i<liczba; i++){
+            for(int j=0; j<liczba; j++){
+                System.out.print("@  ");
+            }
+            System.out.println();
+        }
+
+    }
+}
