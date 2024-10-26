2450bae59614e9161e67f49adfdd5fc597cd68ac
Student <mpodzielny18@gmail.com>
2024-10-26 13:42:42 +0200

    podaj liczbe

---
+++ kurwamac.java
@@ -0,0 +1,12 @@
+import java.until.Scanner;
+
+public.class App {
+    public static void main(String[] args) {
+
+        Scanner input=new Scanner(System.in);
+        System.out.print("Podaj liczbę: ");
+        int liczba=input.nextInt();
+
+        System.out.println("Podana liczba to: " + liczba)
+    }
+}
